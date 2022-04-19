ef0fca2bad613ab470d179a70c52ea363d8a51e5
Comma Device <device@comma.ai>
2022-04-19 23:33:05 +0900

    kf 0.00005

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -242,7 +242,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate == CAR.MOHAVE:
-      ret.lateralTuning.pid.kf = 0.0001
+      ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 2315. + STD_CARGO_KG
       ret.wheelbase = 2.895
 
